e43cb1bc84462d4a0c54b97866665d291da491a6
conrad96 <conrad.mugisha@gmail.com>
2019-08-26 19:39:42 +0300

    create session on success user login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,7 +2,29 @@
 class Login extends CI_Controller{
     
     function index(){
-        parent::__construct();
+        parent:: __construct();
         $this->load->view("guest/index");
+    }
+    function login(){
+        if(!empty($_POST)){
+            $email = $this->input->post("email");
+            $password = $this->input->post("password");
+            $sql = "SELECT * FROM users u WHERE u.email LIKE '".$email."' AND u.password LIKE '".$password."' ";
+            $query = $this->db->query($sql)->result();
+            if(!empty($query)){
+                //set session
+                foreach($query as $user){
+                    $user_data = array(
+                        'userid'=>$user->id,
+                        'names'=>$user->full_names
+
+                    );
+                    $this->session->set_userdata($user_data);
+                }
+            }else{
+                $data['msg'] = 'Incorrect email or password';
+                $this->load->view("guest/login", $data);
+            }
+        }
     }
 }
--- application/views/guest/index.php
+++ application/views/guest/index.php
@@ -1,21 +1,31 @@
-<?php $this->load->view("shared/header");l ?>
+<?php $this->load->view("shared/header"); ?>
 
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="<?php echo base_url(); ?>index.php/login/index"><b>Ticket Processing</b>System</a>
+    <a href="<?php echo base_url(); ?>index.php/login/index">Ticket Processing System</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
-
+    <?php 
+    if(!empty($msg)){
+        print '
+        <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <h4><i class="icon fa fa-ban"></i> Oh snap!</h4>
+                '.$msg.'
+        </div>
+        ';
+    }
+    ?>
     <form action="<?php echo base_url(); ?>index.php/login" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" class="form-control" placeholder="Email" name="email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Password" name="password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
