3552cf76c9f7baf5a010e6b5c687da0defdbeaea
conrad96 <conrad.mugisha@gmail.com>
2019-08-29 23:34:52 +0300

    added ticket widget

--- application/models/_tickets.php
+++ application/models/_tickets.php
@@ -2,11 +2,14 @@
 class _tickets extends CI_Model{
     function get_all_tickets(){
         return $this->db->query("
-        SELECT t.* 
+        SELECT t.ticket,
+                t.description,
+                t.dateadded,
+                u.full_names as author,
+                d.department 
             FROM tickets t 
         INNER JOIN users u ON u.id = t.user_id 
-        LEFT JOIN departments d ON d.id = u.department_id         
-        ")->result();
+            LEFT JOIN departments d ON d.id = u.department_id ")->result();
     }
     function add($data = array()){
         $this->db->insert("tickets", $data);
